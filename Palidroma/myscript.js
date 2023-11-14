2cde2eff60f2e13ba48aa4037eb7a5383e35a1b8
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-14 17:44:18 +0100

    feat: function palindroma

--- Palidroma/myscript.js
+++ Palidroma/myscript.js
@@ -6,13 +6,18 @@ let parolaInvertita="";
 
 for (let i = parolaUtente.length - 1; i >= 0; i--) {
     parolaInvertita += parolaUtente[i];
+
+    
 } console.log (parolaInvertita)
 
 if (parolaInvertita=parolaUtente) {
     alert("La parola è palindroma");
 }
-else if(parolaInvertita !== parolaUtente) {
+ if(parolaInvertita !== parolaUtente) {
     alert("Mi dispiace la parola non è palindroma");
 }
 
 }
+
+
+parolaPalindroma()
